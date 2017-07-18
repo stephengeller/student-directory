6400a26b6e4281eef3e49541f49fd80df295d303
stephengeller <stephen.geller@hotmail.com>
2017-07-18 17:20:50 +0100

    Added input_students method to add students with user

--- directory.rb
+++ directory.rb
@@ -1,3 +1,23 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :july}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+
 #first we print the list of students
 def print_header
   puts "The students of Villains Academy"
@@ -29,6 +49,9 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
